b766ab640c32b7e36cb27904886cb3752c38c103
7ARISTOISCLIENT7 <133221410+7ARISTOISCLIENT7@users.noreply.github.com>
2023-05-22 16:02:51 +0500

    Create MAINSCRIPTLONG.lua

---
+++ MainStuff/MAINSCRIPTLONG.lua
@@ -0,0 +1,20 @@
+local Version = "DBV1"
+local ashversioncheck
+
+function ASHCHECKVERSION()
+    if ashversioncheck == Version then
+        print("Correct Version")
+    else
+        print("Incorrect Version")
+    end
+end
+
+if isfile("/ASHV1/Comfirm.ASHF") then
+    local ashversioncheck = readfile("/ASHV1/Comfirm.ASHF")
+    print("You Have Folder Already Loading")
+else
+    local ashversioncheck = readfile("/ASHV1/Comfirm.ASHF")
+    print("You Dont Have the Folder Making In Workspace It Will Be Called ASHV1")
+    makefolder("/ASHV1")
+    writefile("/ASHV1/Comfirm.ASHF", Version)
+end
